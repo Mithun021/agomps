88b13b9da953609dd635c5486bfc98360be1bc5e
Mithun021 <mithunkr79038@gmail.com>
2025-01-25 19:05:02 +0530

    Admin Panel Setup

--- app/Views/admin/layouts/footer.php
+++ app/Views/admin/layouts/footer.php
@@ -6,11 +6,11 @@
                             <div class="col-12">
                                 <!-- Footer Area -->
                                 <footer class="footer-area d-sm-flex justify-content-center align-items-center justify-content-between">
-                                    <!-- Copywrite Text -->
-                                    <div class="copywrite-text">
+                                    
+                                    <!-- <div class="copywrite-text">
                                         <p>Created by @<a href="#">Theme-zome</a></p>
-                                    </div>
-                                    <div class="fotter-icon text-center">
+                                    </div> -->
+                                    <!-- <div class="fotter-icon text-center">
                                         <a href="#" class="action-item mr-2" data-toggle="tooltip" title="Facebook">
                                             <i class="fa fa-facebook" aria-hidden="true"></i>
                                         </a>
@@ -23,7 +23,7 @@
                                         <a href="#" class="action-item mr-2" data-toggle="tooltip" title="Instagram">
                                             <i class="fa fa-instagram" aria-hidden="true"></i>
                                         </a>
-                                    </div>
+                                    </div> -->
                                 </footer>
                             </div>
                         </div>
