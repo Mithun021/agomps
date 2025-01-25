6156de8d9b009904b10481dffdf02545d64ba8e4
Mithun021 <mithunkr79038@gmail.com>
2025-01-25 18:37:36 +0530

    Admin Panel Setup

--- app/Views/admin/layouts/header.php
+++ app/Views/admin/layouts/header.php
@@ -99,10 +99,7 @@
                     
                     <li class="nav-item">
                         <a class="nav-link" data-toggle="collapse" href="#maps" aria-expanded="false" aria-controls="maps">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin link-icon">
-                                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                                <circle cx="12" cy="10" r="3"></circle>
-                            </svg>
+                            <i class="ti-angle-double-right"></i>
                             <span class="menu-title">Maps</span>
                             <i class="ti-angle-right"></i>
                         </a>
