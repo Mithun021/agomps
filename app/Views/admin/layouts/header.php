11e5937eb8d2f3c4a0a3c48c02256e1d29c0e853
Mithun021 <mithunkr79038@gmail.com>
2025-01-25 18:39:35 +0530

    Admin Panel Setup

--- app/Views/admin/layouts/header.php
+++ app/Views/admin/layouts/header.php
@@ -96,11 +96,13 @@
                             <span class="menu-title">Dashboard</span>
                         </a>
                     </li>
-                    
+
                     <li class="nav-item">
                         <a class="nav-link" data-toggle="collapse" href="#maps" aria-expanded="false" aria-controls="maps">
-                            <i class="ti-angle-double-right"></i>
-                            <svg class="menu-title">Maps</svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-command link-icon">
+                                <path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path>
+                            </svg>
+                            <span class="menu-title">Maps</span>
                             <i class="ti-angle-right"></i>
                         </a>
                         <div class="collapse" id="maps">
