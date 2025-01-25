9c3008d5f1171f168fee1448c4068eae6aa9d833
Mithun021 <mithunkr79038@gmail.com>
2025-01-25 19:09:57 +0530

    Admin Panel Setup

--- app/Views/admin/sports-category.php
+++ app/Views/admin/sports-category.php
@@ -3,12 +3,28 @@
 <div class="card">
     <div class="row">
         <div class="col-lg-4">
-            <form action="">
-                <div class="form-group">
-                    <span>Sports Name</span>
-                    <input type="text" class="form-control" placeholder="Enter sports name" name="sports_name">
+            <div class="card">
+                <div class="card-header">Add Sports Category</div>
+                <form action="">
+                <div class="card-body">
+                        <div class="form-group">
+                            <span>Sports Category Name</span>
+                            <input type="text" class="form-control" placeholder="Enter sports category name" name="sports_category_name" required>
+                        </div>
+                        <div class="form-group">
+                            <span>Sports Category Description</span>
+                            <textarea class="form-control" placeholder="Enter sports category description" name="sports_category_description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <span>Upload Image(JPG,PNG)</span>
+                            <input type="file" class="form-control" name="sports_category_image" accept=".png,.jpg" required>
+                        </div>
                 </div>
-            </form>
+                <div class="card-footer">
+                    <button class="btn btn-primary" type="submit">Add Sports Category</button>
+                </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
