71f9147b9184939cf20cc286e0a3e61336212d07
Mithun021 <mithunkr79038@gmail.com>
2025-01-25 19:00:20 +0530

    Admin Panel Setup

--- app/Views/admin/layouts/header.php
+++ app/Views/admin/layouts/header.php
@@ -108,8 +108,8 @@
                         <div class="collapse" id="maps">
                             <ul class="nav flex-column sub-menu">
                                 <li class="nav-item"> <a class="nav-link" href="<?= base_url() ?>admin/sports-category">Sports Category</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="<?= base_url() ?>league-session">League Session</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="<?= base_url() ?>league-category">League Category</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="<?= base_url() ?>admin/league-session">League Session</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="<?= base_url() ?>admin/league-category">League Category</a></li>
                                 
                             </ul>
                         </div>
