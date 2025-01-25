5464be1fa1f3e8e2486188ce2e3b62485d8c4994
Mithun021 <mithunkr79038@gmail.com>
2025-01-25 20:38:54 +0530

    Admin Panel Setup

--- app/Views/admin/sports-category.php
+++ app/Views/admin/sports-category.php
@@ -60,7 +60,7 @@
                             <td>Edinburgh</td>
                             <td>61</td>
                             <td>
-                                <a href="<?= base_url() ?>admin/delete-sports-category" class="btn btn-sm btn-circle btn-danger"><span class="fa fa-times"></span></a> &nbsp; &nbsp;
+                                <a href="<?= base_url() ?>admin/delete-sports-category" class="btn btn-sm btn-circle btn-danger"><span class="fa fa-times"></span></a> &nbsp;
                                 <a href="<?= base_url() ?>admin/edit-sports-category" class="btn btn-sm btn-circle btn-primary"><span class="fa fa-pencil"></span></a>
                             </td>
                         </tr>
