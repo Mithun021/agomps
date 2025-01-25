771ee839c136233649bc201086b49403a90355be
Mithun021 <mithunkr79038@gmail.com>
2025-01-25 21:56:07 +0530

    Admin Panel Setup

--- app/Views/admin/league-category.php
+++ app/Views/admin/league-category.php
@@ -1,7 +1,9 @@
 <?= $this->extend('admin/layouts/master') ?>
 <?= $this->section('body-content') ?>
 <?php
+
 use App\Models\League_category_model;
+
 $league_category_model = new League_category_model();
 ?>
 <div class="row">
@@ -55,41 +57,43 @@ $league_category_model = new League_category_model();
                 <h4 class="card-title m-0">League Category List</h4>
             </div>
             <div class="card-body">
-                <table id="datatable-buttons" class="table table-striped dt-responsive table-responsive w-100">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>File</th>
-                            <th>Name</th>
-                            <th>Sports Category</th>
-                            <th>Status</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($league_category as $key => $value): ?>
+                <div class="table-responsive">
+                    <table id="datatable-buttons" class="table table-striped dt-responsive">
+                        <thead>
                             <tr>
-                                <td><?= ++$key ?></td>
-                                <td>
-                                    <?php if (!empty($value['featured_image']) && file_exists('public/admin/uploads/league/' . $value['featured_image'])): ?>
-                                        <a href="<?= base_url() ?>public/admin/uploads/league/<?= $value['featured_image'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/uploads/league/<?= $value['featured_image'] ?>" alt="" height="30px"></a>
-                                    <?php else: ?>
-                                        <img src="<?= base_url() ?>public/admin/uploads/league/invalid_image.png" alt="" height="40px">
-                                    <?php endif; ?>
-                                </td>
-                                <td><?= $value['name'] ?></td>
-                                <td><?= $league_category_model->get($value['sports_id'])['name'] ?? '__' ?></td>
-                                <td><?= ($value['status'] == "0") ? "<span class='badge badge-danger badge-pill'>Inactive</span>" : (($value['status'] == "1") ? "<span class='badge badge-success badge-pill'>Active</span>" : "") ?></td>
-                                <td>
-                                <a href="<?= base_url() ?>admin/edit-league-category" class="btn btn-sm btn-circle btn-danger"><span class="fa fa-times"></span></a>
-                                    <a href="<?= base_url() ?>admin/edit-league-category" class="btn btn-sm btn-circle btn-primary"><span class="fa fa-pencil"></span></a>
-                                </td>
+                                <th>#</th>
+                                <th>File</th>
+                                <th>Name</th>
+                                <th>Sports Category</th>
+                                <th>Status</th>
+                                <th>Action</th>
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($league_category as $key => $value): ?>
+                                <tr>
+                                    <td><?= ++$key ?></td>
+                                    <td>
+                                        <?php if (!empty($value['featured_image']) && file_exists('public/admin/uploads/league/' . $value['featured_image'])): ?>
+                                            <a href="<?= base_url() ?>public/admin/uploads/league/<?= $value['featured_image'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/uploads/league/<?= $value['featured_image'] ?>" alt="" height="30px"></a>
+                                        <?php else: ?>
+                                            <img src="<?= base_url() ?>public/admin/uploads/league/invalid_image.png" alt="" height="40px">
+                                        <?php endif; ?>
+                                    </td>
+                                    <td><?= $value['name'] ?></td>
+                                    <td><?= $league_category_model->get($value['sports_id'])['name'] ?? '__' ?></td>
+                                    <td><?= ($value['status'] == "0") ? "<span class='badge badge-danger badge-pill'>Inactive</span>" : (($value['status'] == "1") ? "<span class='badge badge-success badge-pill'>Active</span>" : "") ?></td>
+                                    <td>
+                                        <a href="<?= base_url() ?>admin/edit-league-category" class="btn btn-sm btn-circle btn-danger"><span class="fa fa-times"></span></a>
+                                        <a href="<?= base_url() ?>admin/edit-league-category" class="btn btn-sm btn-circle btn-primary"><span class="fa fa-pencil"></span></a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 </div>
-<?= $this->endSection() ?>  
+<?= $this->endSection() ?>
