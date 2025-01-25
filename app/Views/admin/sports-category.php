5da7cb72252f1cf42c7196234d451de26734da32
Mithun021 <mithunkr79038@gmail.com>
2025-01-25 19:02:27 +0530

    Admin Panel Setup

--- app/Views/admin/sports-category.php
+++ app/Views/admin/sports-category.php
@@ -1,4 +1,15 @@
 <?= $this->extend('admin/layouts/master') ?>
 <?= $this->section('body-content') ?>
-
+<div class="card">
+    <div class="row">
+        <div class="col-lg-4">
+            <form action="">
+                <div class="form-group">
+                    <span>Sports Name</span>
+                    <input type="text" class="form-control" placeholder="Enter sports name" name="sports_name">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <?= $this->endSection() ?>
