fdd9ac18c89cc3d2b520067654501fc54ca46aa8
Mithun021 <mithunkr79038@gmail.com>
2025-01-25 10:16:20 +0530

    Sign in setup

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,6 +1,25 @@
 </div>
 <!-- Content END-->
- 
+ <!-- Modal -->
+<div class="modal fade" id="signinModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 <!-- Footer -->
 <!-- Footer -->
 <footer class="site-footer footer-overlay bg-img-fix" style="background-image: url(<?= base_url() ?>public/assets/images/background/bg5.jpg); background-position: center bottom; background-size: cover; display: block;">
@@ -186,8 +205,8 @@
 
 
     $(document).ready(function () {
-        $('#openexampleModal').click(function () {
-            $('#exampleModal').modal('show');
+        $('#signInBtn').click(function () {
+            $('#signinModal').modal('show');
         });
     });
 </script>
