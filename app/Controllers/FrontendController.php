da47a8bb7776cafd30ac3ab63bdd37aa71d31605
Mithun021 <mithunkr79038@gmail.com>
2025-01-26 15:17:43 +0530

    Admin Panel Setup

--- app/Controllers/FrontendController.php
+++ app/Controllers/FrontendController.php
@@ -17,7 +17,44 @@ class FrontendController extends BaseController
         if ($this->request->is('get')) {
             return view('user-registration', $data);
         } else if ($this->request->is('post')) {
+            $userid = $this->request->getPost('mobile_number');
+            $password = strtoupper($this->request->getPost('first_name')) . '123456';
 
+            $data = [
+                'first_name' => $this->request->getPost('first_name'),
+                'middle_name' => $this->request->getPost('middle_name'),
+                'last_name' => $this->request->getPost('last_name'),
+                'mobile_number' => $this->request->getPost('mobile_number'),
+                'whatsapp_number' => $this->request->getPost('whatsapp_number'),
+                'email_address' => $this->request->getPost('email_address'),
+                'age' => $this->request->getPost('age'),
+                'gender' => $this->request->getPost('gender'),
+                'aadhar' => $this->request->getPost('aadhar'),
+                'state' => $this->request->getPost('state'),
+                'city' => $this->request->getPost('city'),
+                'pincode' => $this->request->getPost('pincode'),
+                'full_address' => $this->request->getPost('full_address'),
+                'password' => password_hash($password, PASSWORD_DEFAULT),
+                'status' => 0
+            ];
+            $result = $players_model->add($data);
+            if ($result == true) {
+                return redirect()->to('user-registration')->with(
+                    'status',
+                    '<div class="alert alert-success" role="alert"> 
+            <h4>Thank you for registering. <br> Below are your login credentials: <br> 
+            User ID: ' . $userid . '<br> 
+            Password: ' . $password . ' </h4>
+        </div>'
+                );
+            } else {
+                return redirect()->to('user-registration')->with(
+                    'status',
+                    '<div class="alert alert-danger" role="alert"> 
+            <h4>' . $result . ' </h4>
+        </div>'
+                );
+            }
         }
     }
 
@@ -114,7 +151,7 @@ class FrontendController extends BaseController
                 return redirect()->to('team-registration')->with(
                     'status',
                     '<div class="alert alert-danger" role="alert"> 
-            <h4>'.$result.' </h4>
+            <h4>' . $result . ' </h4>
         </div>'
                 );
             }
@@ -122,21 +159,21 @@ class FrontendController extends BaseController
     }
 
 
-    public function privacy_policy(){
+    public function privacy_policy()
+    {
         $data = ['title' => 'Privacy Policy'];
-        return view('privacy-policy',$data);
+        return view('privacy-policy', $data);
     }
 
-    public function term_condition(){
+    public function term_condition()
+    {
         $data = ['title' => 'Term Conditions'];
-        return view('term-condition',$data);
+        return view('term-condition', $data);
     }
 
-    public function refund_policy(){
+    public function refund_policy()
+    {
         $data = ['title' => 'Refund Policy'];
-        return view('refund-policy',$data);
+        return view('refund-policy', $data);
     }
-
-
-
 }
