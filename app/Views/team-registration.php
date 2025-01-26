e87ffab2c8de99f698142f9dc51f54494a6d9e1a
Mithun021 <mithunkr79038@gmail.com>
2025-01-26 19:13:32 +0530

    Admin Panel Setup

--- app/Views/team-registration.php
+++ app/Views/team-registration.php
@@ -13,13 +13,13 @@
                         <div class="dez-box p-a20 border-1 bg-gray">
                             <div class="dez-thum-bx dez-img-overlay1 dez-img-effect zoom">
                                 <?php if (!empty($value['sports_image']) && file_exists('public/admin/uploads/sports/' . $value['sports_image'])): ?>
-                                    <a href="<?= base_url() ?>public/admin/uploads/sports/<?= $value['sports_image'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/uploads/sports/<?= $value['sports_image'] ?>" alt=""></a>
+                                    <img src="<?= base_url() ?>public/admin/uploads/sports/<?= $value['sports_image'] ?>" alt="">
                                 <?php else: ?>
                                     <img src="<?= base_url() ?>public/admin/uploads/sports/invalid_image.png" alt="">
                                 <?php endif; ?>
                             </div>
                             <div class="dez-info p-t20 text-center">
-                                <h4 class="dez-title m-t0 m-b10 text-capitalize"><a href="javascript:void(0);">first heading</a></h4>
+                                <h4 class="dez-title m-t0 m-b10 text-capitalize"><a href="<?= base_url() ?>select-league/<?= $value['id'] ?>">first heading</a></h4>
                             </div>
                         </div>
                     </div>
