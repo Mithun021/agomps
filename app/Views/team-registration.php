b92426be4d0cd6946ac2fcf95a9fa073c051fd68
Mithun021 <mithunkr79038@gmail.com>
2025-01-26 19:11:34 +0530

    Admin Panel Setup

--- app/Views/team-registration.php
+++ app/Views/team-registration.php
@@ -8,20 +8,27 @@
     <div class="section-full content-inner">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-sm-6 m-b30">
-                    <div class="dez-box p-a20 border-1 bg-gray">
-                        <div class="dez-thum-bx dez-img-overlay1 dez-img-effect zoom"> <img src="<?= base_url() ?>public/assets/images/product/pic1.jpg" alt="">
-                            <div class="overlay-bx">
-                                <div class="overlay-icon"> <a href="javascript:void(0)"> <i class="fa fa-cart-plus icon-bx-xs"></i> </a> <a href="javascript:void(0)"> <i class="fa fa-search icon-bx-xs"></i> </a> <a href="javascript:void(0)"> <i class="fa fa-heart icon-bx-xs"></i> </a> </div>
+                <?php foreach ($sports as $value) { ?>
+                    <div class="col-lg-4 col-sm-6 m-b30">
+                        <div class="dez-box p-a20 border-1 bg-gray">
+                            <div class="dez-thum-bx dez-img-overlay1 dez-img-effect zoom">
+                                <?php if (!empty($value['sports_image']) && file_exists('public/admin/uploads/sports/' . $value['sports_image'])): ?>
+                                    <a href="<?= base_url() ?>public/admin/uploads/sports/<?= $value['sports_image'] ?>" target="_blank"><img src="<?= base_url() ?>public/admin/uploads/sports/<?= $value['sports_image'] ?>" alt=""></a>
+                                <?php else: ?>
+                                    <img src="<?= base_url() ?>public/admin/uploads/sports/invalid_image.png" alt="">
+                                <?php endif; ?>
+                                <div class="overlay-bx">
+                                    <div class="overlay-icon"> <a href="javascript:void(0)"> <i class="fa fa-cart-plus icon-bx-xs"></i> </a> <a href="javascript:void(0)"> <i class="fa fa-search icon-bx-xs"></i> </a> <a href="javascript:void(0)"> <i class="fa fa-heart icon-bx-xs"></i> </a> </div>
+                                </div>
+                            </div>
+                            <div class="dez-info p-t20 text-center">
+                                <h4 class="dez-title m-t0 m-b10 text-capitalize"><a href="javascript:void(0);">first heading</a></h4>
+                                <h2 class="m-tb0">$20.00 </h2>
+                                <a href="javascript:void(0)" class="site-button  m-t15">Add To Cart</a>
                             </div>
                         </div>
-                        <div class="dez-info p-t20 text-center">
-                            <h4 class="dez-title m-t0 m-b10 text-capitalize"><a href="javascript:void(0);">first heading</a></h4>
-                            <h2 class="m-tb0">$20.00 </h2>
-                            <a href="javascript:void(0)" class="site-button  m-t15">Add To Cart</a>
-                        </div>
                     </div>
-                </div>
+                <?php } ?>
             </div>
         </div>
     </div>
