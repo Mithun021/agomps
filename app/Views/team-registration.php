8a954a38845cbf23f3414e226bd5d4102a3938af
Mithun021 <mithunkr79038@gmail.com>
2025-01-27 13:14:57 +0530

    Course List

--- app/Views/team-registration.php
+++ app/Views/team-registration.php
@@ -1,12 +1,14 @@
-<?= $this->extend("layouts/master") ?>
-<?= $this->section("body-content"); ?>
-<?= view('layouts/breadcumbs') ?>
-
-<!-- contact area -->
-<div class="clearfix">
-    <!-- Product -->
-    <div class="section-full content-inner">
-        <div class="container">
+<!-- Our Achievements -->
+<div class="section-full bg-white content-inner our-achievements">
+    <div class="container">
+        <div class="section-head text-center ">
+            <h2 class="h2 text-uppercase"> Well-Managed Sports Leagues</h2>
+            <div class="dez-separator-outer ">
+                <div class="dez-separator bg-primary style-liner"></div>
+            </div>
+            <p>Well-managed sports leagues ensure fair competition, seamless organization, and a professional environment, providing athletes with opportunities to thrive.</p>
+        </div>
+        <div class="section-content text-center ">
             <div class="row m-lr0">
                 <?php foreach ($sports as $value) { ?>
                     <div class="col-lg-3 col-sm-6 p-a0 m-b30">
@@ -19,9 +21,7 @@
                             <!-- <img width="292" height="292" src="<?= base_url() ?>public/assets/images/sports/pic1.jpg" alt=""> -->
                             <div class="dez-info-has p-a20 bg-primary text-left skew-triangle right-top text-center">
                                 <h4 class="text-capitalize"> <?= $value['name'] ?></h4>
-                                <div class="dez-info-has-text"><?php if (!empty($value['description'])) {
-                                                                    echo $value['description'];
-                                                                } ?></div>
+                                <div class="dez-info-has-text"><?php if(!empty($value['description'])){  echo $value['description']; } ?></div>
                                 <div class="m-tb30"><a href="<?= base_url() ?>select-league/<?= $value['id'] ?>" class="site-button outline white border-1">Read More</a></div>
                             </div>
                             <div class="dez-title-bx bg-gray p-a20 text-left skew-triangle left-top">
@@ -31,27 +31,6 @@
                     </div>
                 <?php } ?>
             </div>
-            <div class="row">
-                <?php foreach ($sports as $value) { ?>
-                    <div class="col-lg-4 col-sm-6 m-b30">
-                        <div class="dez-box p-a20 border-1 bg-gray">
-                            <div class="dez-thum-bx dez-img-overlay1 dez-img-effect zoom">
-                                <?php if (!empty($value['sports_image']) && file_exists('public/admin/uploads/sports/' . $value['sports_image'])): ?>
-                                    <img src="<?= base_url() ?>public/admin/uploads/sports/<?= $value['sports_image'] ?>" alt="">
-                                <?php else: ?>
-                                    <img src="<?= base_url() ?>public/admin/uploads/sports/invalid_image.png" alt="">
-                                <?php endif; ?>
-                            </div>
-                            <div class="dez-info p-t20 text-center">
-                                <h4 class="dez-title m-t0 m-b10 text-capitalize"><a href="<?= base_url() ?>select-league/<?= $value['id'] ?>"><?= $value['name'] ?></a></h4>
-                                <?php if (!empty($value['description'])) { ?><p><?= $value['description'] ?></p><?php } ?>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
-            </div>
         </div>
     </div>
 </div>
-
-<?= $this->endSection() ?>
