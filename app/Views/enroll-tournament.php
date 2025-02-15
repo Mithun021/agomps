1b866a249d83dca5a05f25b78650cd5484b2692c
Mithun021 <mithunkr79038@gmail.com>
2025-02-15 13:12:22 +0530

    Tournamant

--- app/Views/enroll-tournament.php
+++ app/Views/enroll-tournament.php
@@ -182,9 +182,9 @@ if ($sessionData) {
                                                             <?php for ($i = 1; $i <= $tournaments['max_players']; $i++) { ?>
                                                                 <tr id="teamRow<?= $i ?>">
                                                                     <td><?= $i ?></td>
-                                                                    <td><input type="text" class="form-control player_name" name="player_name[]" placeholder="Enter Player Name" required></td>
-                                                                    <td><input type="number" name="player_age[]" class="form-control player_age" placeholder="Player Age" required></td>
-                                                                    <td><input type="text" name="player_mobileno[]" class="form-control player_mobileno" placeholder="Player Mobile no." maxlength="10" required pattern="^[6-9][0-9]{9}$"></td>
+                                                                    <td><input type="text" class="form-control player_name" name="player_name[]" placeholder="Enter Player Name"></td>
+                                                                    <td><input type="number" name="player_age[]" class="form-control player_age" placeholder="Player Age"></td>
+                                                                    <td><input type="text" name="player_mobileno[]" class="form-control player_mobileno" placeholder="Player Mobile no." maxlength="10" pattern="^[6-9][0-9]{9}$"></td>
                                                                 </tr>
                                                             <?php } ?>
                                                         </tbody>
