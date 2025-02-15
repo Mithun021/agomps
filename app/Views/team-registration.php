ddee6be829f73644a9b2b2ccd0a84959b2fc5935
Mithun021 <mithunkr79038@gmail.com>
2025-02-15 10:39:14 +0530

    Tournamant

--- app/Views/team-registration.php
+++ app/Views/team-registration.php
@@ -5,13 +5,69 @@
 <?php
 
 use App\Models\Sports_model;
+use App\Models\Game_category_model;
+use App\Models\League_session_model;
+use App\Models\Sports_subcategory_model;
+use App\Models\Tournament_model;
 
 $sports_model = new Sports_model();
 $sports = $sports_model->getActiveData();
+$game_category_model = new Game_category_model();
+$game_category = $game_category_model->get();
+$league_session_model = new League_session_model();
+$league_session = $league_session_model->currectSession();
+$tournament_model = new Tournament_model();
+$sports_subcategory_model = new Sports_subcategory_model();
 ?>
+
 <style>
-    .enrollprice span{
+    .enrollprice span {
         font-weight: 600;
+    }
+
+    img.sports_icon {
+        height: 18px !important;
+    }
+
+    .joinColor {
+        color: #0559db;
+        font-weight: bold;
+        font-style: italic;
+        font-size: 12px;
+    }
+
+    .tournamentDateColor {
+        color: #f42b0c;
+        font-weight: bold;
+        font-style: italic;
+        font-size: 12px;
+    }
+
+    .image_with_price{
+        position: relative;
+    }
+    .image_with_price
+    .winColor {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        background-color:rgba(255, 255, 255, 0.58);
+        width: 100%;
+        color: #7307d3;
+        font-weight: bold;
+        font-style: italic;
+        font-size: 14px;
+        text-align: center;
+    }
+
+    .divider {
+        border: 1px solid rgb(81, 80, 80);
+    }
+
+    h5.card-title {
+        font-size: 24px;
+        font-weight: bold;
+        margin-top: 5px;
     }
 </style>
 
@@ -25,32 +81,109 @@ $sports = $sports_model->getActiveData();
             </div>
             <p>Well-managed sports leagues ensure fair competition, seamless organization, and a professional environment, providing athletes with opportunities to thrive.</p>
         </div>
-        <div class="section-content text-center ">
+        <div class="section-content ">
             <div class="row m-lr0">
-                <?php foreach ($sports as $value) { ?>
-                    <div class="col-lg-3 col-sm-6 p-a0 m-b30">
-                        <div class="dez-box dez-media">
-                            <?php if (!empty($value['sports_image']) && file_exists('public/admin/uploads/sports/' . $value['sports_image'])): ?>
-                                <img width="292" height="292" src="<?= base_url() ?>public/admin/uploads/sports/<?= $value['sports_image'] ?>" alt="">
-                            <?php else: ?>
-                                <img width="292" height="292" src="<?= base_url() ?>public/admin/uploads/sports/invalid_image.png" alt="">
-                            <?php endif; ?>
-                            <!-- <img width="292" height="292" src="<?= base_url() ?>public/assets/images/sports/pic1.jpg" alt=""> -->
-                            <div class="dez-info-has p-a20 bg-primary text-left skew-triangle right-top text-center">
-                                <h4 class="text-capitalize"> <?= $value['name'] ?></h4>
-                                <div class="dez-info-has-text"><?php if(!empty($value['description'])){  echo $value['description']; } ?></div>
-                                <div class="m-tb30"><a href="<?= base_url() ?>select-league/<?= $value['id'] ?>" class="site-button outline white border-1">Read More</a></div>
+
+                <div class="dez-tabs border bg-tabs">
+
+                    <ul class="nav nav-tabs" id="myTabContent" role="tablist">
+                        <?php
+                        $isFirst = true;
+                        foreach ($sports as $value) {
+                        ?>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link <?php echo $isFirst ? 'active' : ''; ?>"
+                                    id="sportsCat<?= $value['id'] ?>"
+                                    data-bs-toggle="tab"
+                                    data-bs-target="#sports-<?= $value['id'] ?>"
+                                    type="button" role="tab"
+                                    aria-controls="sports-<?= $value['id'] ?>"
+                                    aria-selected="<?= $isFirst ? 'true' : 'false' ?>">
+                                    <img src="<?= base_url() ?>public/admin/uploads/sports/<?= $value['sports_image'] ?>" alt="<?= $value['name'] ?> Icon" class="sports_icon">
+                                    <span class="title-head"><?= $value['name'] ?></span>
+                                </button>
+                            </li>
+                            <?php $isFirst = false; ?> <!-- Once the first tab is rendered, the flag is set to false -->
+                        <?php } ?>
+                    </ul>
+
+                    <div class="tab-content" id="myTabContent">
+                        <?php
+                        $isFirst = true; // Flag to set the first tab as active by default
+                        foreach ($sports as $value) {
+                        ?>
+
+                            <div class="tab-pane fade <?php echo $isFirst ? 'show active' : ''; ?>" id="sports-<?= $value['id'] ?>" role="tabpanel" aria-labelledby="sportsCat<?= $value['id'] ?>" tabindex="0">
+
+                                <!-- Game Category  -->
+                                <?php if ($game_category) {
+                                    foreach ($game_category as $key => $gcat) {
+                                        echo "<h1>" . $gcat['game_category'] . "</h1>";
+                                        $tournament = $tournament_model->get_by_category($league_session['id'], $value['id'], $gcat['game_category']);
+                                        echo "<div class='row'>";
+                                        foreach ($tournament as $key => $tournament) { ?>
+                                            <div class="col-lg-4 col-md-6">
+                                                <div class="card p-0 mb-3">
+                                                    <a href="<?= base_url() ?>enroll-tournament">
+                                                    <div class="image_with_price">
+                                                        <?php if (!empty($tournament['featured_image']) && file_exists('public/admin/uploads/tournament/' . $tournament['featured_image'])): ?>
+                                                            <img src="<?= base_url() ?>public/admin/uploads/tournament/<?= $tournament['featured_image'] ?>" alt="" class="card-img-top">
+                                                        <?php else: ?>
+                                                            <img src="<?= base_url() ?>public/admin/uploads/invalid_image.jpg" alt="" class="card-img-top">
+                                                        <?php endif; ?>
+
+                                                        <p class="winColor m-0"><i class="fa fa-trophy"></i> Winning Guarantee : <i class="fa fa-inr"></i> <?= $tournament['first_rank_price'] ?></p>
+                                                    </div>
+                                                    <div class="card-body py-1">
+                                                        <div class="d-flex justify-content-between mb-1">
+                                                            <p class="joinColor m-0"><i class="fa fa-user-plus"></i> Join : <i class="fa fa-inr"></i>
+                                                                <?php
+                                                                if ($tournament['game_type'] == "Individual") {
+                                                                    $registration_fee = $tournament['registration_fee'];
+                                                                    $discount_registration_fee = $tournament['discount_registration_fee'];
+                                                                    $tournament_price = $registration_fee;
+                                                                    if (!empty($discount_registration_fee) && $discount_registration_fee < $registration_fee) {
+                                                                        $tournament_price = $discount_registration_fee;
+                                                                    }
+                                                                    echo $tournament_price;
+                                                                } else if ($tournament['game_type'] == "Team") {
+                                                                    $registration_fee = $tournament['team_entry_fee'];
+                                                                    $discount_registration_fee = $tournament['team_entry_fee_discount'];
+                                                                    $tournament_price = $registration_fee;
+                                                                    if (!empty($discount_registration_fee) && $discount_registration_fee < $registration_fee) {
+                                                                        $tournament_price = $discount_registration_fee;
+                                                                    }
+                                                                    echo $tournament_price;
+                                                                }
+                                                                ?>
+                                                            </p>
+
+                                                            <p class="tournamentDateColor m-0"><i class="fa fa-calendar"></i> End : <?php echo date("d-M-y", strtotime($league_session['end_date'])); ?></p>
+                                                        </div>
+                                                        <hr class="divider m-0">
+                                                        <h5 class="card-title text-center"><?= $tournament['league_for'] ?? '#NA' ?> <?= $sports_subcategory_model->get($tournament['sport_subcategory'])['sub_category_name'] ?? '' ?> <?= $value['name'] ?></h5>
+                                                    </div>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                <?php }
+                                        echo "</div>";
+
+                                        // echo "<pre>"; print_r($tournament);
+                                    }
+                                }
+
+
+                                ?>
+
+
                             </div>
-                            <div class="dez-title-bx bg-gray p-a20 text-left skew-triangle left-top">
-                                <h4 class="m-a0 text-capitalize"> <?= $value['name'] ?></h4><hr class="my-1">
-                                <div class="enrollprice d-flex justify-content-between">
-                                    <span class="text-info"><i class="fa fa-hand-o-right"></i> Join : <i class="fa fa-rupee"></i> <?= $value['joining_amount'] ?></span>
-                                    <span class="text-primary"><i class="fa fa-trophy"></i> Win : <i class="fa fa-rupee"></i> <?= $value['winning_amount'] ?></span>
-                                </div>
-                            </div>
-                        </div>
+                        <?php $isFirst = false;
+                        } ?>
                     </div>
-                <?php } ?>
+                </div>
+
+
             </div>
         </div>
     </div>
