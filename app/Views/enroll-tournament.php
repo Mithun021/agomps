1f401a8b4bdf0cef74fe70ade8e6d75246c218f7
Mithun021 <mithunkr79038@gmail.com>
2025-02-15 11:01:02 +0530

    Tournamant

--- app/Views/enroll-tournament.php
+++ app/Views/enroll-tournament.php
@@ -1,6 +1,91 @@
 <?= $this->extend("layouts/master") ?>
 <?= $this->section("body-content"); ?>
 <?= view('layouts/breadcumbs') ?>
+<?php
+$sessionData = session()->get('loggedPlayerData');
+if ($sessionData) {
+    $loggedplayerId = $sessionData['loggedplayerId'];
+}
 
+?>
+
+<style>
+    form span {
+        font-weight: 600;
+    }
+
+    form input,
+    form select {
+        width: 100%;
+        height: 40px !important;
+    }
+
+    table {
+        width: 100%;
+        border: 1px solid #BABABA;
+    }
+
+    table thead {
+        background-color: #232323;
+        color: #FFF;
+    }
+
+    table thead tr td,
+    table tbody tr td {
+        border: 1px solid #BABABA;
+        border-collapse: collapse;
+        padding: 5px;
+    }
+
+    #addTeamTable #teamBody #teamRow:first-child td:last-child span {
+        display: none;
+    }
+
+    .registerFormBody {
+        border: 5px solid #ffc107;
+    }
+    img.payment_qrcode{
+        width: 100% !important;
+        height: 240px !important;
+        object-fit: cover;
+    }
+</style>
+
+<!-- contact area -->
+<div class="clearfix">
+    <!-- Product -->
+    <div class="section-full content-inner">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card registerFormBody">
+                        <?php
+                        if (session()->getFlashdata('status')) {
+                            echo session()->getFlashdata('status');
+                        }
+                        ?>
+                        <?php
+                        $sessionData = session()->get('loggedPlayerData');
+                        if ($sessionData) {
+                            $loggedplayerName = $sessionData['loggedplayerName'];
+                            $loggedplayerId = $sessionData['loggedplayerId'];
+                        }
+
+                        if (!isset($sessionData)) {
+                        ?>
+                            <div class="card-body p-2 text-danger">
+                                <h3>You must <span class="text-primary">log in</span> first to complete the registration form.</h3>
+                            </div>
+                        <?php
+                        }
+                        ?>
+                        <div class="card-header p-0"><img src="<?= base_url() ?>public/assets/images/background/registration.jpg" alt=""></div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?= $this->endSection() ?>
