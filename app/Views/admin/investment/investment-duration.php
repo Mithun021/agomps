f9a3d5cc009609e4fc30d42f8e10f071b6e4b844
Mithun021 <mithunkr79038@gmail.com>
2025-02-16 17:50:47 +0530

    Investment

--- app/Views/admin/investment/investment-duration.php
+++ app/Views/admin/investment/investment-duration.php
@@ -68,7 +68,7 @@ $investment_plan_model = new Investment_plan_model();
                         <tr>
                             <td><?= ++$key ?></td>
                             <td><?= $investment_plan_model->get($value['investment_type_id'])['plan_type'] ?? '' ?></td>
-                            <td><?= $value['duration'] ?></td>
+                            <td><?= $value['duration'] ?> <?= $value['notes'] ?></td>
                             <td>
                                 <a href="<?= base_url() ?>admin/edit-investment-plan" class="btn btn-sm btn-circle btn-danger"><span class="fa fa-times"></span></a>
                                 <a href="<?= base_url() ?>admin/delete-investment-plan" class="btn btn-sm btn-circle btn-primary"><span class="fa fa-pencil"></span></a>
