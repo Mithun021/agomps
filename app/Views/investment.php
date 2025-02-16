ce960ba810c890ffa6c413f37ea3c80dcdc8e903
Mithun021 <mithunkr79038@gmail.com>
2025-02-17 00:31:31 +0530

    Investment

--- app/Views/investment.php
+++ app/Views/investment.php
@@ -6,7 +6,7 @@
 <div class="section-full bg-white content-inner our-achievements">
     <div class="container">
         <div class="section-content ">
-            <div class="row m-lr0">
+            <div class="row col-set-block white-block m-lr0">
                 <div class="col-lg-12">
                     <div class="dez-head-bx m-a-out m-b20 skew-triangle right-top">
                         <h3 class="m-a0">Latest Match</h3>
