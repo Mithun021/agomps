d846dbb1106f4c698e3e100accf2d6e9a7f88f2c
Mithun021 <mithunkr79038@gmail.com>
2025-02-16 14:10:52 +0530

    Display Sports

--- app/Controllers/FrontendController.php
+++ app/Controllers/FrontendController.php
@@ -128,7 +128,7 @@ class FrontendController extends BaseController
         if ($this->request->is('get')) {
             $title['title'] = 'Home';
             if(!isset($sessionData)){
-                session()->setFlashdata('alert', '<div class="alert alert-success" role="alert">Account not found! Please log in again.</div>');
+                session()->setFlashdata('alert', '<div class="alert alert-success" role="alert">Account not found! Please log in you account first.</div>');
                 return view('index',$title);
             }
 
