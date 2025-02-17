3139a0d93567ad444ef928187c87122dfb06dd95
Mithun021 <mithunkr79038@gmail.com>
2025-02-17 07:35:24 +0530

    Display Sports

--- app/Views/investment-details.php
+++ app/Views/investment-details.php
@@ -2,4 +2,19 @@
 <?= $this->section("body-content"); ?>
 <?= view('layouts/breadcumbs') ?>
 
+<!-- Our Achievements -->
+<div class="section-full bg-white content-inner our-achievements">
+    <div class="container">
+        <div class="section-content ">
+            <div class="row col-set-block white-block m-lr0">
+                    <div class="col-lg-8">
+                        <div class="dez-head-bx m-a-out m-b20 skew-triangle right-top">
+                            <h3 class="m-a0"><?= $investment['title'] ?> - <i class="fa fa-inr"></i> <?= $investment['min_amount'] ?> Investment</h3>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?= $this->endSection() ?>
